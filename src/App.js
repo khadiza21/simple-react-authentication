e55967c80e2a81ca6d1966d18afe8c4b87a2f34f
khadiza21 <bibikhadiza474@gmail.com>
2022-04-12 07:18:51 +0600

    react firebase google authentication

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
 import "./App.css";
+import app from "../src/firebase.init";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useState } from "react";
+
+const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({});
+  const provider = new GoogleAuthProvider(); //pop up provider
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // The signed-in user info.
+        const user = result.user;
+        console.log(user);
+        setUser(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const handleGoogleSignOut = () => {
+    signInWithPopup(auth, provider);
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        setUser({});
+      })
+      .catch((error) => {
+        // An error happened.
+        setUser({});
+      });
+  };
+
   return (
     <div className="App">
-      <h1>This is P</h1>
+      {user.email ? (
+        <button onClick={handleGoogleSignOut}>Sign Out</button>
+      ) : (
+        <button onClick={handleGoogleSignIn}>Google</button>
+      )}
+      <br />
+      <br />
+      <h2>Name: {user.displayName}</h2>
+      <h2>Name: {user.email}</h2>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyBrSzekGWMhyd56HcrIefynBPujXNEEIBo",
+    authDomain: "simple-react-authenticat-1ff42.firebaseapp.com",
+    projectId: "simple-react-authenticat-1ff42",
+    storageBucket: "simple-react-authenticat-1ff42.appspot.com",
+    messagingSenderId: "806779851326",
+    appId: "1:806779851326:web:ba384e9dc99ab78822376f"
+  };
+
+  
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
